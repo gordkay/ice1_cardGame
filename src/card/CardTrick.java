7c5b318d987cd19d6ba6e71b415d8e4553c0d2dc
Kayla Gordon <gordkayl@sheridancollege.ca>
2024-09-24 14:06:32 -0400

    ask user to pick a card

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,6 +4,9 @@
  */
 package card;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,18 +18,47 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random rand = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            //insert call to random number generator here
+            c.setValue(rand.nextInt(13) + 1);
+            //insert call to random number between 0-3 here
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
+        //insert code to ask the user for Card value and suit, 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a card value (1 to 13): ");
+        int userValue = scanner.nextInt();
+        System.out.println("Pick a suit (Hearts, Diamonds, Clubs, Spades): ");
+        String userSuit = scanner.next();
+        //create their card
+        Card userCard = new Card();
+        userCard.setValue(userValue);
+        userCard.setSuit(userSuit);
         // and search magicHand here
+        boolean found = false;
+        for (Card card : magicHand){
+        if (card.getValue() == userCard.getValue() && card.getSuit().equalsIgnoreCase(userCard.getSuit())){
+        found = true;
+        break;
+        }
+        }
         //Then report the result here
+        if (found){
+        System.out.println("This is your card");
+        }
+        else
+        {
+                System.out.println("This is not your card");
+                }
+    }
         // add one luckcard hard code 2,clubs
+       
     }
     
-}
+
