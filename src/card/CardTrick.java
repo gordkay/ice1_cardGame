5e22df4c06ad743ccbf2832515ebf01d35653ff2
gordkay <134324431+gordkay@users.noreply.github.com>
2024-09-24 14:27:38 -0400

    added luckyCard

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+*Modifier: Kayla Gordon
+*Student Number: 991659941
+*/
+
 package card;
 
 import java.util.Random;
@@ -57,8 +62,28 @@ public class CardTrick {
                 System.out.println("This is not your card");
                 }
     }
+
+    
         // add one luckcard hard code 2,clubs
-       
+       Card luckyCard = new Card();
+    luckyCard.setValue(7);
+luckyCard.setSuit("Diamonds");
+
+ // Search for the lucky card in the magicHand array
+        boolean foundLucky = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equalsIgnoreCase(luckyCard.getSuit())) {
+                foundLucky = true;
+                break;
+            }
+        }
+// Report if the lucky card was found
+        if (foundLucky) {
+            System.out.println("Lucky card found! You win!");
+        } else {
+            System.out.println("Lucky card not found. You lose.");
+        }
+    }
     }
     
 
